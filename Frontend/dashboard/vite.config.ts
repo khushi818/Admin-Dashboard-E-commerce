c90d755ed4d7d4e9c8e828ec839abd2e26db1d26
Khushi Shukla <khushishukla5890@gmail.com>
2023-09-18 08:53:54 +0530

    production setting

--- Frontend/dashboard/vite.config.ts
+++ Frontend/dashboard/vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isDev = process.env.NODE_ENV === "development";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api/v1": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
+        target: isDev
+          ? "http://localhost:8000"
+          : "https://admin-dashboard-0hge.onrender.com",
+        changeOrigin: isDev,
+        secure: !isDev,
         ws: true,
       },
     },
